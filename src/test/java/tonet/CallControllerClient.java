ae9f3716d6df2102934754e4b5e7a17877ef7533
ecmel <ecmel.ercan@gmail.com>
2020-10-26 12:23:28 +0300

    Fixes

--- src/test/java/tonet/CallControllerClient.java
+++ src/test/java/tonet/CallControllerClient.java
@@ -9,5 +9,5 @@ import io.micronaut.http.client.annotation.Client;
 public interface CallControllerClient
 {
     @Post
-    public HttpResponse<String> generateToken(@Body CallOptions options);
+    HttpResponse<String> generateToken(@Body CallOptions options);
 }
