7270617bc6027559b21b4aab2564561fca0c1302
ecmel <ecmel.ercan@gmail.com>
2020-11-04 23:02:06 +0300

    Fixes

--- src/test/java/tonet/AbstractContainerTest.java
+++ src/test/java/tonet/AbstractContainerTest.java
@@ -9,21 +9,22 @@ import io.micronaut.test.support.TestPropertyProvider;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class AbstractContainerTest implements TestPropertyProvider
 {
-    static final String image = "openvidu/openvidu-server-kms:latest";
-    static final int port = 4443;
     static final GenericContainer<?> container;
 
     static
     {
-        container = new GenericContainer<>(image);
-        container.withExposedPorts(port).start();
+        container = new GenericContainer<>("openvidu/openvidu-server-kms:latest");
+
+        container
+            .withExposedPorts(4443)
+            .start();
     }
 
     @Override
     public Map<String, String> getProperties()
     {
         Map<String, String> map = new HashMap<>();
-        map.put("openvidu.port", container.getMappedPort(port).toString());
+        map.put("openvidu.port", container.getFirstMappedPort().toString());
         return map;
     }
 }
